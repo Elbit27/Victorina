fe70bb138d3f1709b9b96dd04f4a5bb81e4c8222
elbur27 <mamatzhanov1397@gmail.com>
2026-02-20 16:21:18 +0600

    updated entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input && echo "Collectstatic completed successfully"
+# Останавливаем скрипт при любой ошибке
+set -e
 
-# Создаём суперпользователя, если он ещё не существует
+echo "--> Applying migrations..."
+python manage.py migrate --no-input
+
+echo "--> Collecting static..."
+python manage.py collectstatic --no-input
+
+echo "--> Creating superuser..."
 python manage.py shell << EOF
 import os
 from django.contrib.auth import get_user_model
 User = get_user_model()
-password = os.environ.get("SUPERUSERS_PASSWORD", "admin123")
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", password)
+username = "admin"
+password = os.environ.get("SUPERUSER_PASSWORD", "27")
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, "admin@example.com", password)
+    print(f"Superuser {username} created successfully.")
+else:
+    print(f"Superuser {username} already exists.")
 EOF
 
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+# ВАЖНО: Используем Daphne для поддержки WebSocket
+# И используем переменную $PORT, которую дает Render
+echo "--> Starting Server..."
+exec daphne -b 0.0.0.0 -p $PORT config.asgi:application
