e874b499a1e6a23695be951de2fd44e117616dfc
elbur27 <mamatzhanov1397@gmail.com>
2026-03-31 18:45:30 +0600

    fixed issue in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# Останавливаем скрипт при любой ошибке
 set -e
 
 echo "--> Starting Server with Daphne..."
-exec daphne -u /tmp/daphne.sock -b 0.0.0.0 -p $PORT config.asgi:application
+exec daphne -b 0.0.0.0 -p $PORT config.asgi:application
